44eb3906da916fae1fd0ddf5c914777d39efa47a
Ashu <ashuyadav210905@gmail.com>
2026-06-06 12:41:29 +0530

    Update sqlqueries2.sql

---
+++ sqlqueries2.sql
@@ -0,0 +1,47 @@
+create database if not exists sqlprac;
+use sqlprac;
+create table college(
+rollno int primary key ,
+name varchar(25),
+marks int not null,
+grade varchar(1),
+city varchar(10)
+);
+
+insert into college(rollno,name,marks,grade,city) values 
+(101,"ashu",89,"A","surat"),
+(102,"jayu",81,"A","pune"),
+(103,"keshav",79,"b","city1"),
+(104,"uv",99,"A","surat"),
+(105,"harsh",69,"c","surat"),
+(106,"tanish",72,"b","pune");
+
+select * from college;
+show tables;
+show databases;
+
+drop table  if exists table2; 
+
+select name , rollno from college;
+
+select distinct city from college;
+select distinct grade,city from college;
+
+select* from college where marks+10>90 or city = "surat";
+select* from college where city = "city2";
+
+select * from college where marks between 81 and 89;
+select * from college where city in ("surat","pune");
+select * from college where marks  not between 81 and 89;
+select * from college where city  not in ("surat","pune");
+select * from college where city like "%t";
+select * from college where city like "_u%";
+
+select * from college limit 3;
+
+select * from college where city like "_u%" limit 3;
+
+
+select * from college 
+order by marks desc 
+limit 3;
