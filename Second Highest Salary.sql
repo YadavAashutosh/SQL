fbe401118e9484b3ee140dc97503cdd691cb520d
Ashu <ashuyadav210905@gmail.com>
2026-06-08 14:11:13 +0530

    dsa in sql

---
+++ Second Highest Salary.sql
@@ -0,0 +1,9 @@
+--176. Second Highest Salary
+
+# Write your MySQL query statement below
+select ( select salary 
+from Employee
+where salary< (select max(salary) from Employee)
+order by salary desc
+limit 1)
+as SecondHighestSalary ;
