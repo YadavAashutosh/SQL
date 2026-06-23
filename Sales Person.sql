b753d95d3ab51326d30e7a0a57797a2b17188ed4
Ashu <ashuyadav210905@gmail.com>
2026-06-23 16:23:34 +0530

    Create Sales Person.sql

---
+++ Sales Person.sql
@@ -0,0 +1,7 @@
+-- 607. Sales Person
+
+# Write your MySQL query statement below
+select name from SalesPerson
+where sales_id not in (select sales_id from Orders
+where com_id = ( select com_id from Company
+where name = "RED"))
